ea99ba583e3ab565e1a5fbfcab1bce4b95982e81
Heikki Kupiainen <heikki.kupiainen@oppikone.fi>
2018-08-01 09:56:47 +0300

    added car server example and ui side login logic

--- src/store/MetaStore.js
+++ src/store/MetaStore.js
@@ -1,12 +1,15 @@
 import {dispatch, handle} from 'synchronous-dispatcher'
 import axios from 'axios';
-import {CAR_DATA_URL} from '../constants';
+import {CAR_DATA_URL, LOGIN_URL} from '../constants';
 
 export const REQUEST_CAR_DATA = 'REQUEST_CAR_DATA';
 export const CAR_DATA_CHANGE = 'CAR_DATA_CHANGE';
 export const LOAD_CAR_DATA = 'CAR_DATA_CHANGE';
 export const LOAD_CAR_DATA_SUCCESS = 'LOAD_CAR_DATA_SUCCESS';
 export const LOAD_CAR_DATA_ERROR = 'LOAD_CAR_DATA_ERROR';
+export const SUBMIT_LOGIN = 'SUBMIT_LOGIN';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
 const metaStore = {
   carDetails: null
@@ -28,8 +31,17 @@ export const initMetaStore = () => {
     }).catch(error => dispatch(LOAD_CAR_DATA_ERROR, error));
   });
 
-  handle('LOAD-CAR-DATA-SUCCESS', (data) => {
+  handle(LOAD_CAR_DATA_SUCCESS, (data) => {
     metaStore.carDetails = data;
     dispatch(CAR_DATA_CHANGE, {...metaStore.carDetails});
   });
+
+  handle(SUBMIT_LOGIN, (credentials) => {
+    axios.post(LOGIN_URL, credentials)
+    .then(response => {
+      metaStore.accessToken = response.data;
+      dispatch(LOGIN_SUCCESS, metaStore.accessToken)
+    }).catch(error => dispatch(LOGIN_FAILURE, error));
+  });
+
 };
