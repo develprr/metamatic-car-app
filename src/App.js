078ad45ec4be3710aa8b27a895dbd0623177f932
Heikki Kupiainen <heikki.kupiainen@oppikone.fi>
2018-08-01 10:20:17 +0300

    integrated App.js main class with MetaStore

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+import {AppTitle} from './component/AppTitle.js';
+import {Management} from './component/Management.js';
+import {Login} from './component/Login.js';
+
+import {handle} from 'synchronous-dispatcher';
+import {initMetaStore, LOGIN_STATE_CHANGE, LOGOUT} from './store/MetaStore';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    initMetaStore();
+    this.state = {loggedIn: true};
+    handle(LOGIN_STATE_CHANGE, (loggedIn) => {
+        this.setState({loggedIn});
+    })
+  }
+
+  handleLoginSuccess(event) {
+    this.setState({loggedIn: true});
+  }
+
+  createViewComponent() {
+    if (this.state.loggedIn) {
+      return <Management/>;
+    } else {
+      return <Login/>;
+    }
+  }
+
+  render() {
+    let viewComponent = this.createViewComponent();
+    return (
+        <div className="container-fluid">
+          <AppTitle name="Cars"/>
+          {viewComponent}
+        </div>
+    );
+  }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
+
+export default App;
