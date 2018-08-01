b71b9dfd7bfe73e499e554d93f797a047d0f4869
Heikki Kupiainen <heikki.kupiainen@oppikone.fi>
2018-08-01 17:10:49 +0300

    further simplified filter function implementation

--- src/service/CarModelService.js
+++ src/service/CarModelService.js
@@ -1,6 +1,6 @@
 export class CarModelService {
 
   static filterByModel = (models, keyword) => Object.values(models).filter(
-      (carModel) => carModel.model.toLowerCase().indexOf(keyword.toLowerCase()) > -1 ? true : false);
+      (carModel) => carModel.model.toLowerCase().indexOf(keyword.toLowerCase()) > -1);
 
 }
