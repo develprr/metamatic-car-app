4994a52778af805ba54f9ce82802881bc88da8f5
Heikki Kupiainen <heikki.kupiainen@oppikone.fi>
2018-08-01 16:30:47 +0300

    changed CarModelList to use MetaStore directly instead of props

--- src/component/CarModelList.js
+++ src/component/CarModelList.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import {dispatch} from 'synchronous-dispatcher';
-import {CAR_MODEL_SELECTED} from '../store/MetaStore';
+import {dispatch, handle} from 'synchronous-dispatcher';
+import {CAR_LIST_CHANGE, CAR_MODEL_SELECTED, REQUEST_CAR_LIST} from '../store/MetaStore';
 
 export class CarModelList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      cars: []
+    };
+    handle(CAR_LIST_CHANGE, this.constructor.name, (cars) => {
+      this.setState({cars: Object.values(cars)});
+    });
+  }
+
+  componentDidMount() {
+    dispatch(REQUEST_CAR_LIST);
+  }
+
   render() {
-    var itemMap = this.props.items.map((item) =>
-      <li className="list-group-item" data-id={item.id} onClick={() => {dispatch(CAR_MODEL_SELECTED, item.id)}} key={item.name.toString()}>
-        {item.name}
+    const cars = this.state.cars;
+    const itemMap = cars.map((item) =>
+      <li className="list-group-item" data-id={item.id} onClick={() => {dispatch(CAR_MODEL_SELECTED, item.id)}} key={item.model.toString()}>
+        {item.model}
       </li>
     );
     return (
