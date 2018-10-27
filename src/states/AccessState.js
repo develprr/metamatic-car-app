ef30dcf53846386b3e85f7533e5d34bc800a8bd0
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-10-27 16:34:04 +0300

    added comments

--- src/states/AccessState.js
+++ src/states/AccessState.js
@@ -1,9 +1,10 @@
 import {initState, updateState} from 'metamatic'
 import {ACCESS_STATE} from '../config/states';
 
+//use initState instead of setState or updateState to set initial state, so the state state won't be reset on browser page reload.
 export const AccessState = () => initState(ACCESS_STATE, {
   loggedIn: false
-})
+});
 
 export const logout = () => updateState(ACCESS_STATE, {
   loggedIn: false
