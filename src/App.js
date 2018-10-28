cad59243530f22522637d3a0100f569a85948eba
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-10-28 17:00:10 +0200

    renamed AccessState -> AuthorizationState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
-import {Management} from './component/Management.js';
 import Login from './component/Login.js';
 import {connect, disconnect} from 'metamatic';
-import {ACCESS_STATE} from './config/states';
+import {STATE_AUTHORIZATION} from './config/states';
 import {CarFilterList} from './component/CarFilterList';
 import {CarDetails} from './component/CarDetails';
 import {Route} from 'react-router-dom';
@@ -17,10 +16,10 @@ export class App extends React.Component {
     this.state = {};
   }
 
-  componentDidMount = () => connect(this, ACCESS_STATE, (state) => this.setState(state));
+  componentDidMount = () => connect(this, STATE_AUTHORIZATION, (state) => this.setState(state));
 
   componentWillUnmount = () => disconnect(this);
-  
+
   isLoggedIn = () => this.state.loggedIn === true;
 
   renderAuthorizedContent = () => (
@@ -46,4 +45,3 @@ export class App extends React.Component {
       </BrowserRouter>
   )
 }
-
