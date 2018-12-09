f2c5112b99e7592e8e2b4130f82cb412e2848188
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-12-09 17:52:54 +0200

    added order car button

--- src/component/CarDetails.js
+++ src/component/CarDetails.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {connectToStore, disconnectFromStores} from 'metamatic';
 import {STORE_CAR_MODEL_ITEM} from '../stores/CarModelStore';
+import withRouter from 'react-router-dom/withRouter';
 
-export class CarDetails extends React.Component {
+class CarDetails extends React.Component {
 
   constructor(props) {
     super(props);
@@ -13,16 +14,30 @@ export class CarDetails extends React.Component {
 
   componentWillUnmount = () => disconnectFromStores(this);
 
+  getCarModelDetails = () => this.state.carModelDetails;
+
+  onOrderButtonClicked = () => {
+    const details = this.getCarModelDetails();
+    const carModelId = details.id;
+    this.props.history.push(`/cars/${carModelId}/order`);
+  }
+
   render() {
-    let carDetails = this.state.carModelDetails;
+    const carDetails = this.getCarModelDetails();
     return carDetails ? (
+      <form>
         <ul className="list-group">
           <li className="list-group-item"><h3>{carDetails.model}</h3></li>
           <li className="list-group-item">Top Speed: {carDetails.speed}</li>
           <li className="list-group-item">Color: {carDetails.color}</li>
           <li className="list-group-item">Price: €{carDetails.price}</li>
         </ul>
+        <button id="order-button" type="button" className="btn btn-default btn-lg pull-right" onClick={this.onOrderButtonClicked}>
+          <span className="glyphicon glyphicon-eur" aria-hidden="true"></span> Order now
+        </button>
+      </form>
     ) : null;
 
   }
 }
+export default withRouter(CarDetails);
