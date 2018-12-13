cc4f88c1678be256e81f49ebd968715cb74e3db8
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-12-13 14:53:25 +0200

    made car app to use demo server by default

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-const HOST = window.location.href.startsWith('http://localhost:5000') ? 'http://localhost:5000' : 'https://metamatic-demo-server.herokuapp.com';
+const HOST = 'https://metamatic-demo-server.herokuapp.com';
 
 export const CAR_DATA_URL = `${HOST}/api/cars`;
 export const LOGIN_URL = `${HOST}/api/login`;
